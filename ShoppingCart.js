7628c570fe988e99f182eabffa3c87b7b720b559
Meenakshi S <93512809+Meenakshi-Rvkmr@users.noreply.github.com>
2021-11-16 16:21:52 +0530

    update1

---
+++ ShoppingCart.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  ajax();
+});
+function ajax() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      //document.getElementById("list").innerHTML = this.response;
+
+      $("#example").DataTable({
+        data: JSON.parse(this.response).data,
+        columns: [
+            {},
+          { data: "SNo" },
+          { data: "Item" },
+          { data: "Quantity" },
+          { data: "Unit" },
+          { data: "Department" },
+        ],
+        columnDefs: [
+          {
+            targets: 0,
+            searchable: false,
+            orderable: false,
+            className: "dt-body-center",
+            render: function (data, type, full, meta) {
+              return (
+                '<input type="checkbox" name="id[]" value="' +
+                $("<div/>").text(data).html() +
+                '">'
+              );
+            },
+          },
+        ],
+      });
+    }
+  };
+  xhttp.open("GET", "grocerylist.json", true);
+  xhttp.send();
+}
